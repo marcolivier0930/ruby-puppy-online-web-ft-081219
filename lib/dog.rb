02aeb3738f72778836eb211e7e0e70715e45e4cb
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-24 13:36:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Add your code here
+class Dog 
+  
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  def self.all
+    @@all 
+  end
+  def self.print_all
+    @@all.each do |dog|
+      puts dog 
+    end
+  end
+end
